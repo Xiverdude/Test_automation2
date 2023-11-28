a8cd37e4b0ef43e36000ba950989ed17cb14e95f
Denis Illarionov <xiverden@gmail.com>
2023-11-28 13:35:39 +0300

    another one

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,11 +3,15 @@ package org.example;
 //РАБОЧИЙ АВТОТЕСТ
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.time.Duration;
+
+//img[@src='https://hhcdn.ru/ichameleon/271669.jpeg']  пример xpath
+//*[@src='https://hhcdn.ru/ichameleon/271669.jpeg']
 
 public class Main {
     public static void main(String[] args) {
@@ -17,13 +21,27 @@ public class Main {
         System.setProperty(driverPath, chromePath);  //передаем этот путь в качестве параметра при создании объекта WebDriver:
         WebDriver driver = new ChromeDriver();       //создаем новый драйвер
         //driver.manage().window().maximize();    //делаем окно браузера на весь экран
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
-        driver.get("https://www.youtube.com/");
+        //driver.manage().window().setSize(new Dimension(900, 500)); //указываем размер окна
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(1));//неявное ожидание
+        driver.get("https://www.google.ru/");
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(1));
+        driver.navigate().to("https://www.selenium.dev/");// указываем ссылку, на которую перейти
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(1));
+        driver.navigate().back();//возвращаемся на предыдущую страницу
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(1));
+        driver.navigate().forward();//вернуться обратно на страницу
+        driver.navigate().refresh();
 
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-        WebElement input = driver.findElement(By.cssSelector("#container .style-scope.ytd-searchbox"));
-        input.sendKeys("Гантели");
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+        System.out.println(driver.getTitle());// получаем тайтл
+        System.out.println(driver.getCurrentUrl());// получим url страницы и выведем в консоли
+
+
+        driver.quit();
+
+
+
+
+
 
     }
 }
