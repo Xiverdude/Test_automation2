28a07e1a784874933c3b8e3fe741ab42668626d2
Denis Illarionov <xiverden@gmail.com>
2023-11-28 22:47:25 +0300

    enough for today

--- src/main/java/org/example/RadioButton.java
+++ src/main/java/org/example/RadioButton.java
@@ -1,7 +1,12 @@
 package org.example;
-
+//CHECKBOX
+//RADIOBUTTON
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
 
 public class RadioButton {
     public static void main(String[] args) {
@@ -10,6 +15,15 @@ public class RadioButton {
 
         System.setProperty(driverPath, chromePath);  //передаем этот путь в качестве параметра при создании объекта WebDriver:
         WebDriver driver = new ChromeDriver();       //создаем новый драйвер
+        driver.navigate().to("https://market.yandex.ru/");
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
+        driver.findElement(By.xpath("//input[@type='text']")).sendKeys("стиральная машина" + Keys.ENTER);//вводим тескт в описковую строку и нажимает ENTER
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+        driver.findElement(By.xpath("//div[@data-filter-value-id='44150331']//span[@class='_1ZDAA']")).click();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(7));
+        driver.findElement(By.xpath("//div[@data-filter-value-id='44150331']//span[@class='_1ZDAA']")).click();
+
+
 
 
     }
